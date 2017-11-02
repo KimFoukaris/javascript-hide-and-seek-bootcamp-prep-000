dd2cb334958742ddcfc4b56e775a73647263f79f
Kim Foukaris <kimfoukaris@outlook.com>
2017-11-02 17:29:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function getFirstSelector(selector) {
+  const select = document.querySelector(selector)
+  return select
+}
+
+function nestedTarget() {
+  const target = document.querySelector('#nested .target')
+  return target
+  }
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let i=0; i<lis.length; i++) {
+    var temp = parseInt(lis[i].innerHTML) + parseInt(n)
+    lis[i].innerHTML = temp
+  }
+  return lis
+}
+
+function deepestChild() {
+  const children = document.getElementById('grand-node').querySelectorAll('div')
+  var temp = ""
+    for (let i=0; i<children.length; i++) {
+
+      if (children[i] != null) {
+        temp = children[i]
+      }
+    }
+  return temp
+}
