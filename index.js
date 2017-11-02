aa9e6befb29c1782b132bdf837906602f6140245
Kim Foukaris <kimfoukaris@outlook.com>
2017-11-02 16:17:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,17 @@ function nestedTarget() {
 function increaseRankBy(n) {
   const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
   for (let i=0; i<lis.length; i++) {
-    var temp = parseInt(lis[i].innerHTML)
-    temp = temp + n
+    var temp = parseInt(lis[i].innerHTML) + parseInt(n)
     lis[i].innerHTML = temp
-  return lis
   }
+  return lis
+}
+
+function deepestChild() {
+  const children = document.getElementById('grand-node').querySelectorAll('div')
+  var temp = ""
+    for (let i=o; i<children.length; i++) {
+      if (children[i] != null)
+        var temp = children[i].innerHTML
+    }
 }
