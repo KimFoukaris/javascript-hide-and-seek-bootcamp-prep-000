4e03ad357e6822c5c511e170873ddb5475f85068
Kim Foukaris <kimfoukaris@outlook.com>
2017-11-02 16:18:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ function deepestChild() {
   const children = document.getElementById('grand-node').querySelectorAll('div')
   var temp = ""
     for (let i=0; i<children.length; i++) {
-      if (children[i] != null)
+      if (children[i] != null) {
         var temp = children[i].innerHTML
+      }
     }
 }
